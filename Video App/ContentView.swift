6a432355b16872fc9907ed34d22ecb52d145155d
Frank S. Bara <fbara1@yahoo.com>
2020-02-15 00:05:47 -0600

    Create video player.

--- Video App/ContentView.swift
+++ Video App/ContentView.swift
@@ -7,10 +7,36 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var isVideoPlaying = false
+    let urlString = "https://devstreaming-cdn.apple.com/videos/wwdc/2019/219s60i7y7dovs8r4/219/hls_vod_mvp.m3u8"
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, Video!")
+            
+            Button(action: play) {
+                Image(systemName: "play")
+            }
+        }
+    }
+    
+    func play() {
+        isVideoPlaying.toggle()
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        let player = AVPlayer(url: url)
+        
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        UIApplication.shared.windows.first?.rootViewController?.present(controller, animated: true) {
+            player.play()
+        }
     }
 }
 
